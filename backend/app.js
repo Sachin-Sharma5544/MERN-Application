8dbd73c2e4f1749f5146385b3b221bd92062960d
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2023-03-04 18:30:53 +0530

    Post method added, with mongoose save, promise and async await examples

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const workoutsRoute = require("./routes/workouts");
 
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
@@ -8,14 +9,23 @@ const app = express();
 
 //This middleware helps us read data trom the body
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     //This middleware to print the method type and path
     console.log(req.method, req.path);
     next();
 });
+
 app.use("/workouts", workoutsRoute);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to database");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,3 +1,5 @@
+const Workout = require("../models/workouts");
+
 const router = require("express").Router();
 
 //Get All Workouts
@@ -11,8 +13,31 @@ router.get("/:id", (req, res) => {
 });
 
 //Post a new Workout
-router.post("/", (req, res) => {
+router.post("/", (req, res, next) => {
+    const { title, reps, load } = req.body;
+
+    const workout = new Workout({ title, reps, load });
+    workout.save().then((data) => {
+        console.log(data);
+    });
+
     res.json({ mssg: "Record added successfully" });
+
+    // Workout.create({ title, reps, load })
+    //     .then((data) => {
+    //         console.log(data);
+    //         res.status(201).json(data);
+    //     })
+    //     .catch((error) => {
+    //         res.status(400).json({ error: error.message });
+    //     });
+
+    // try {
+    //     const workout = await Workout.create({ title, load, reps });
+    //     res.status(200).json(workout);
+    // } catch (error) {
+    //     res.status(400).json({ error: error.message });
+    // }
 });
 
 //Post a new Workout
